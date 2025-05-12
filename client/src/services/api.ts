7b9dae10e1c146c6d80a8c972b2e2b0982f5397c
Elpina <elpinaoc@gmail.com>
2025-05-13 11:08:19 +1200

    set up api to amazon rds

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_URL = 'http://localhost:5033/api/meterreadings'; // Adjust if your backend runs on a different port
-const API_URL = 'https://tmj4gpwtik.ap-southeast-2.awsapprunner.com/api/meterreadings';
+const API_URL = 'http://localhost:5033/api/meterreadings'; // Adjust if your backend runs on a different port
+//const API_URL = 'https://tmj4gpwtik.ap-southeast-2.awsapprunner.com/api/meterreadings';
 
 export const getReadings = () => axios.get(API_URL);
 export const postReading = (data: any) => axios.post(API_URL, data);
